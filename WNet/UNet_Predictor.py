238929bd754f5ca098249c46fe578e6d6edce33d
Léo Dupire <leodupire@b-10-27-85-127.dynapool.vpn.nyu.edu>
2023-04-27 23:55:12 -0400

    WNet training added

---
+++ WNet/UNet_Predictor.py
@@ -0,0 +1,116 @@
+# Code inspired by: https://pyimagesearch.com/2021/11/08/u-net-training-image-segmentation-models-in-pytorch/
+
+import torch
+from torch import nn
+
+#@title Conv Block
+class conv_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        return x
+    
+#@title Encoder Block
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = conv_block(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+
+        return x, p
+    
+#@title Decoder Block
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = conv_block(out_c+out_c, out_c)
+
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+
+        return x
+    
+#@title UNet Block
+class UNet_Predictor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embed_dim = 32
+        self.emb = nn.Embedding(49, self.embed_dim)
+
+        """ Encoder """
+        self.e1 = encoder_block(11*self.embed_dim, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
+
+        """ Bottleneck """
+        self.b = conv_block(512, 1024)
+
+        """ Decoder """
+        self.d1 = decoder_block(1024, 512)
+        self.d2 = decoder_block(512, 256)
+        self.d3 = decoder_block(256, 128)
+        self.d4 = decoder_block(128, 64)
+
+        """ Classifier """
+        self.outputs = nn.Conv2d(64, 49, kernel_size=1, padding=0)
+
+    def forward(self, inputs):
+        check = True
+
+        for i in range(11): # Process and reduce each image in sequence
+          temp = inputs[:, i, :, :]  # B x H x W
+          temp = self.emb(temp)  # B x H x W x 32
+          temp = temp.permute(0, 3, 1, 2)  # B x 32 x H x W 
+
+          if check:
+            x = temp
+            check = False
+          else:
+            x = torch.cat((x, temp), dim=1)
+
+        """ Encoder """
+        s1, p1 = self.e1(x)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        """ Bottleneck """
+        b = self.b(p4)
+
+        """ Decoder """
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        """ Classifier """
+        outputs = self.outputs(d4).unsqueeze(1)
+
+        return outputs
